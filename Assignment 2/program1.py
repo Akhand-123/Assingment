e41e4869aa0af8b94362754f1b513e0906ed1b10
Akhand <akhand789ww@gmail.com>
2023-07-07 16:41:42 +0530

    First commit

---
+++ Assignment 2/program1.py
@@ -0,0 +1,17 @@
+# Write a Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples
+
+# Sample List : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+
+# Expected Result : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
+
+sample_list = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+def last(n):
+    return n[-1]
+def tuple_sort(n):
+    x  = sorted(n,key=last)
+    return x
+print(tuple_sort(sample_list))
+
+# Output:
+# [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
+
